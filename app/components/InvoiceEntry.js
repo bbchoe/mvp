83a63dffd488647a395042fa3e7d8a350d04a8e1
Brian Choe <brian@visibook.com>
2017-11-11 12:32:08 -0800

    finished building out invoice data entry form HTML

--- app/components/InvoiceEntry.js
+++ app/components/InvoiceEntry.js
@@ -3,6 +3,22 @@ import React from 'react'
 class InvoiceEntry extends React.Component {
   constructor() {
     super();
+    this.state = {
+      number: '',
+      name: '',
+      code: '',
+      issued: '',
+      due: '',
+      amount: '',
+      expected: '',
+      actual: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
   handleSubmit(event) {
     alert('SUBMIT CLICKED');
@@ -17,49 +33,49 @@ class InvoiceEntry extends React.Component {
             <label>
             Invoice number:
             </label>
-            <input type="text" value="Invoice number" required />
+            <input type="text" name="number" value={this.state.number} onChange={this.handleInputChange} />
           </div>
           <div>
             <label>
             Client name:
             </label>
-            <input type="text" value="Client name" required />
+            <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
           </div>
           <div>
             <label>
             Client code:
             </label>
-            <input type="text" value="Client code" required />
+            <input type="text" name="code" value={this.state.code} onChange={this.handleInputChange} />
           </div>
           <div>
             <label>
             Issued date:
             </label>
-            <input type="text" value="Issued date" required />
+            <input type="date" name="issued" value={this.state.issued} onChange={this.handleInputChange} />
           </div>
           <div>
             <label>
             Due date:
             </label>
-            <input type="text" value="Due date" required />
+            <input type="date" name="due" value={this.state.due} onChange={this.handleInputChange} />
           </div>
           <div>
             <label>
-            Amount due:
+            Amount due:  $
             </label>
-            <input type="text" value="Amount due" required />
+            <input name="amount" type="number" min="0" step="1" value={this.state.amount} onChange={this.handleInputChange} />
           </div>
           <div>
             <label>
             Expected collection timing:
             </label>
-            <input type="text" value="Date" required />
+            <input name="expected" type="date" value={this.state.expected} onChange={this.handleInputChange} />
           </div>
           <div>
             <label>
             Actual collection timing:
             </label>
-            <input type="text" value="Date (if collected)" required />
+            <input name="actual" type="date" value={this.state.actual} onChange={this.handleInputChange} />
           </div>
             <input type="submit" value="Submit" />
         </form>
@@ -67,6 +83,5 @@ class InvoiceEntry extends React.Component {
     )
   }
 }
-
 
 export default InvoiceEntry;
